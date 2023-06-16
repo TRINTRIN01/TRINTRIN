9605be39ffb5d8bb5f705993f110253938fdd45e
João Davis1 <12102040@aluno.cotemig.com.br>
2023-06-16 11:47:40 -0300

    Manutenção tela cadastro bike #01

--- Pit/View/CadastroBike.php
+++ Pit/View/CadastroBike.php
@@ -1,3 +1,19 @@
+<?php
+     
+     if(isset($_POST['submit']))
+     {
+
+    include_once('config.php');
+
+      $nomeBicicleta = $_POST['nomeBicicleta'];
+      $nomeLocador = $_POST['nomeLocador'];
+      $valor = $_POST['valor'];
+      $tel = $_POST['telefone'];
+
+      $result = mysqli_query($conexao, "INSERT INTO Bicicleta(nomeBicicleta,nomeLocador,valor,telefone)
+       VALUES('$nomeBicicleta','$nome','$senha','$endereco','$tel')");
+     }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +22,8 @@
     <link rel="stylesheet" href="estilo.css">
     <title>Cadastro Bicicleta - TRINTRIN</title>
 </head>
-<body id="fundoReserva">
-    <footer id="topoSite">
+<body id="fundoCadBike">
+    <nav id="topoSite">
         <div id="tituloSite"><img src="imagens/iconTrin.png" alt="" id="iconTrin"></div>
         <div id="areaPesquisa"><input type="text" id="Pesquisa" placeholder="Pesquise aqui..."></div>
         <div id="areaIcons">
@@ -18,10 +34,10 @@
                 <img src="imagens/modoEscuro.png" alt="" id="botao-cor1" onclick="voltarCor()">
             </div>
         </div>
-    </footer>
+    </nav>
 
-    <header id="areaCont">
-        <form action="" id="formCadastroBike">
+    <header id="areaConteudoCadBike">
+        <form enctype="multipart/form-data" action="" method="POST" id="formCadastroBike">
             <div id="areaImagemProduto">
                 <label class="picture" for="picture__input" tabIndex="0">
                     <span class="picture__image"></span>
@@ -36,11 +52,22 @@
                 <input type="text" id="nomeLocador" name="nomeLocador" class="inputEntrada" placeholder="nome do locador" required>
                 <input type="text" id="valorAluguel" name="valorAluguel" class="inputEntrada" placeholder="Valor do aluguel" required>
                 <input type="text" id="tel" name="tel" class="inputEntrada" placeholder="Telefone p/ contato" oninput="mascara_telefone()" required>
+                <div id="areaBtnCadBike"><button type="submit" name="submit" id="btnCadBike">Cadastrar</button></div>
             </div>
         </form>
         
     </header>
+
+    <footer id="rodape">
+        <div id="areaLinksPag">
+            <a href="" class="linkPaginaSite">contato</a>
+            <a href="Cadastro.php" class="linkPaginaSite">cadastro</a>
+            <a href="Login.php" class="linkPaginaSite">login</a>
+            <a href="Reserva.php" class="linkPaginaSite">reserva</a>
+        </div>
+
+        <div id="areaLogoSite"><img src="imagens/iconTrin.png" alt="" id="iconTrinRodape"></div>
+    </footer>
 </body>
-<script src="JS/script.js"></script>
-<script src="JS/scriptCadBike"></script>
+<script src="JS/scriptCadBike.js"></script>
 </html>
--- Pit/View/JS/scriptCadBike.js
+++ Pit/View/JS/scriptCadBike.js
@@ -1,3 +1,35 @@
+botaoCor = document.getElementById("botao-cor");
+botaoCor1 = document.getElementById("botao-cor1");
+FundoCadBike = document.getElementById("fundoCadBike");
+barraPesquisa = document.getElementById("Pesquisa");
+linksPag = document.getElementsByClassName("linkPaginaSite");
+
+function trocaCor() {
+  botaoCor.style.display = "none";
+  botaoCor1.style.display = "grid";
+  linksPag[0].style.color = "#ffffff";
+  linksPag[1].style.color = "#ffffff";
+  linksPag[2].style.color = "#ffffff";
+  linksPag[3].style.color = "#ffffff";
+  fundoCadBike.style.backgroundColor = "#000000";
+  barraPesquisa.style.backgroundColor = "#ffffff";
+  barraPesquisa.style.placeholder.color = "#000000"; 
+  
+}
+
+function voltarCor() {
+  botaoCor.style.display = "grid";
+  botaoCor1.style.display = "none";
+  linksPag[0].style.color = "#000000";
+  linksPag[1].style.color = "#000000";
+  linksPag[2].style.color = "#000000";
+  linksPag[3].style.color = "#000000";
+  fundoCadBike.style.backgroundColor = "#ffffff";
+  barraPesquisa.style.backgroundColor = "#990D35";
+}
+
+/*#################### JS Input Imagem #########################
+################################################################*/
 const inputFile = document.querySelector("#picture__input");
 const pictureImage = document.querySelector(".picture__image");
 const pictureImageTxt = "Choose an image";
@@ -26,3 +58,41 @@ inputFile.addEventListener("change", function (e) {
     pictureImage.innerHTML = pictureImageTxt;
   }
 });
+
+function mascara_telefone ()
+        {
+           //limitador
+         var tel = document.getElementById("tel").value
+            console.log(tel)
+          tel=tel.slice(0,14) //(pode limitar a quantidade de char na entrada pelo java script)
+            console.log(tel)
+          document.getElementById("tel").value=tel
+     tel=document.getElementById("tel").value.slice(0,10)
+            console.log(tel)
+           
+            //máscara
+            var tel_formatado = document.getElementById("tel").value
+            if (tel_formatado[0]!="(")
+            {
+                if(tel_formatado[0]!=undefined)
+                {
+                    document.getElementById("tel").value="("+tel_formatado[0];
+                }
+            }
+
+            if (tel_formatado[3]!=")")
+            {
+                if(tel_formatado[3]!=undefined)
+                {
+                    document.getElementById("tel").value=tel_formatado.slice(0,3)+")"+tel_formatado[3]
+                }
+            }
+
+            if (tel_formatado[9]!="-")
+            {
+                if(tel_formatado[9]!=undefined)
+                {
+                    document.getElementById("tel").value=tel_formatado.slice(0,9)+"-"+tel_formatado[9]
+                }
+            }
+        }
