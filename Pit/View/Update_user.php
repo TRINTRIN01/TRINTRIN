5250479ad3dd067ddc6ab4996a4bbdab2cb35ba6
João Davis1 <12102040@aluno.cotemig.com.br>
2023-07-05 11:49:38 -0300

    Começo da implantação do AtuPerfil

--- Pit/View/Update_user.php
+++ Pit/View/Update_user.php
@@ -1,38 +1,4 @@
 
 <?php
-if(!empty($_POST['AtuPerfil']))
-{
-    include_once('config.php');
-    $email  = $conexao->escape_string($_POST['email']);
-    $sqlSelect = "SELECT usuario_id from usuario where email = '$email';";
-    $result = $conexao->query($sqlSelect); 
 
-    if ($result) {
-        // A consulta foi executada sem erros
-        if ($result->num_rows > 0) {
-            // A consulta retornou resultados
-            $row = $result->fetch_assoc(); // Obter a primeira linha do resultado
-            $id = $row['usuario_id']; // Obter o valor da coluna 'id'
-        
-            $sqlUpdate = "UPDATE FROM usuario  SET email = $email, nome = $nome , pass = $senha, endereco = $endereco tel = $tel  where usuario_id = $id;";
-            $resultUp = $conexao->query($sqlUpdate);
-        
-            if ($resultUp) {
-                // O registro foi Alterado com sucesso
-                echo "Registro alterado com sucesso.";
-                header('location: index.html'); 
-            } else {
-                // Ocorreu um erro ao excluir o registro
-                echo "Erro ao Alterar o registro: " . $conexao->error;
-            }
-        } else {
-            // A consulta não retornou resultados
-            echo "Nenhum registro encontrado com o email informado.";
-        }
-    } else {
-        // Ocorreu um erro na consulta
-        echo "Erro na consulta: " . $conexao->error;
-    }
-    
-}
 ?>
--- Pit/View/deleteUser.php
+++ Pit/View/deleteUser.php
@@ -1,8 +1,11 @@
 
 <?php
+
+include_once('config.php');
+
 if(!empty($_POST['ExcConta']))
 {
-    include_once('config.php');
+    
     $email  = $conexao->escape_string($_POST['email']);
     $sqlSelect = "SELECT usuario_id from usuario where email = '$email';";
     $result = $conexao->query($sqlSelect); 
@@ -28,6 +31,46 @@ if(!empty($_POST['ExcConta']))
         } else {
             // A consulta não retornou resultados
             echo "Nenhuma conta encontrada com o email informado.";
+        }
+    } else {
+        // Ocorreu um erro na consulta
+        echo "Erro na consulta: " . $conexao->error;
+    }
+    
+}
+
+if(!empty($_POST['AtuPerfil']))
+{
+    include_once('config.php');
+    $email  = $conexao->escape_string($_POST['email']);
+    $sqlSelect = "SELECT usuario_id from usuario where email = '$email';";
+    $result = $conexao->query($sqlSelect); 
+    $nome = $_POST['nome'];
+    $senha = $_POST['pass'];
+    $endereco = $_POST['endereco'];
+    $tel = $_POST['tel'];
+
+    if ($result) {
+        // A consulta foi executada sem erros
+        if ($result->num_rows > 0) {
+            // A consulta retornou resultados
+            $row = $result->fetch_assoc(); // Obter a primeira linha do resultado
+            $id = $row['usuario_id']; // Obter o valor da coluna 'id'
+        
+            $sqlUpdate = "UPDATE usuario SET nome = '$nome', pass = '$senha', endereco = '$endereco', tel = '$tel'  WHERE email = '$email'";
+            $resultUp = $conexao->query($sqlUpdate);
+        
+            if ($resultUp) {
+                // O registro foi Alterado com sucesso
+                echo "Registro alterado com sucesso.";
+                header('location: index.html'); 
+            } else {
+                // Ocorreu um erro ao excluir o registro
+                echo "Erro ao Alterar o registro: " . $conexao->error;
+            }
+        } else {
+            // A consulta não retornou resultados
+            echo "Nenhum registro encontrado com o email informado.";
         }
     } else {
         // Ocorreu um erro na consulta
