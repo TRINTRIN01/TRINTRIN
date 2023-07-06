a66b05a178cf9f44729be41aaaf6a1720e2a2a0e
João Davis1 <12102040@aluno.cotemig.com.br>
2023-07-06 11:03:57 -0300

    Adição do favicon

--- Pit/View/CadastroBike.php
+++ Pit/View/CadastroBike.php
@@ -34,6 +34,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="imagens/favicon.png">
     <link rel="stylesheet" href="estilo.css">
     <title>Cadastro Bicicleta - TRINTRIN</title>
 </head>
--- Pit/View/CadastroL.php
+++ Pit/View/CadastroL.php
@@ -42,6 +42,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="imagens/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo.css">
     <title>Cadastro</title>
